9d59052f81753f81cec6c456c26b8da8bab74741
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-06-10 17:36:36 -0300

    Adds intro field to Post model

--- leticiastallone/blog/models.py
+++ leticiastallone/blog/models.py
@@ -5,6 +5,7 @@ from taggit.managers import TaggableManager
 
 class Post(models.Model):
     title = models.CharField(max_length=60)
+    intro = models.TextField()
     body = models.TextField()
     tags = TaggableManager(blank=True)
     is_published = models.BooleanField(default=False)
