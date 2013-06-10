23204e5bd6c577819131d3a5a8d75d986fcd638a
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-06-10 17:26:43 -0300

    Fixes filebroweser configs, enable /media for local development

--- leticiastallone/leticiastallone/urls.py
+++ leticiastallone/leticiastallone/urls.py
@@ -1,9 +1,13 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.views.generic import TemplateView
+
+from filebrowser.sites import site
+
 
 admin.autodiscover()
+
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
@@ -17,9 +21,15 @@ urlpatterns = patterns('',
 
     # Grapelli admin interface
     url(r'^grappelli/', include('grappelli.urls')),
-    url(r'^admin/filebrowser/', include('filebrowser.sites.site.urls')),
+    url(r'^admin/filebrowser/', include(site.urls)),
 
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    # static files (images, css, javascript, etc.)
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT}))
