596af3cd54a59dfa46512d85676a24162de36f8e
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-06-11 10:47:56 -0300

    Removes content labels

--- leticiastallone/content/models.py
+++ leticiastallone/content/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 
 class Content(models.Model):
-    title = models.CharField(u'Título', max_length=200, blank=False, null=False)
-    body = models.TextField(u'Texto', blank=False, null=False)
-    slug = models.SlugField(u'Slug', max_length=200, null=False, blank=False)
+    title = models.CharField(max_length=200, blank=False, null=False)
+    body = models.TextField(blank=False, null=False)
+    slug = models.SlugField(max_length=200, null=False, blank=False)
     is_published = models.BooleanField(default=False)
 
     class Meta:
