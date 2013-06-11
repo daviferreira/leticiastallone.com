9fc7098abc9a40b0bcbc2ea4e72516d5daa2625c
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-06-11 17:31:48 -0300

    Removes requirement from Content abstract, adds publication styles

--- leticiastallone/content/models.py
+++ leticiastallone/content/models.py
@@ -8,7 +8,7 @@ from taggit.managers import TaggableManager
 
 class Content(models.Model):
     title = models.CharField(max_length=200, blank=False, null=False)
-    abstract = models.TextField(blank=False, null=False)
+    abstract = models.TextField(blank=True, null=True)
     body = models.TextField(blank=True, null=True)
     slug = models.SlugField(max_length=200, null=False, blank=False)
     pub_date = models.DateTimeField(blank=False, null=False, default=datetime.now())
