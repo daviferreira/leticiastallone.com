f10237b1b4e3b5911a6133ea67adffd9c30690cb
Davi Ferreira <davi.ferreira@corp.globo.com>
2013-06-11 12:19:46 -0300

    Finishes static layout

--- layout/Gruntfile.js
+++ layout/Gruntfile.js
@@ -2,6 +2,20 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    jslint: {
+      files: ['javascripts/leticia.js'],
+      directives: {
+        browser: true,
+        unparam: true,
+        todo: true
+      }
+    },
+    uglify: {
+      build: {
+        src: 'javascripts/leticia.js',
+        dest: 'javascripts/leticia.min.js'
+      }
+    },
     compass: {
       dist: {
         options: {
@@ -22,6 +36,13 @@ module.exports = function(grunt) {
       }
     },
     watch: {
+      scripts: {
+        files: ['javascripts/leticia.js'],
+        tasks: ['js'],
+        options: {
+          debounceDelay: 250,
+        }
+      },
       styles: {
         files: 'sass/**/*.scss',
         tasks: ['css'],
@@ -35,8 +56,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-jslint');
 
+  grunt.registerTask('js', ['jslint', 'uglify']);
   grunt.registerTask('css', ['compass', 'csslint'])
-  grunt.registerTask('default', ['css']);
-
+  grunt.registerTask('default', ['css', 'js']);
 };
---
+++ layout/javascripts/leticia.js
@@ -0,0 +1,31 @@
+var timer,
+    doc = document.documentElement,
+    body = document.body,
+    isFixed = false,
+    max = 100,
+    el = document.getElementById('masthead'),
+    contentEl = document.getElementById('main-content'),
+    setFixedHeader = function setFixedHeader() {
+        'use strict';
+        var scrollTop = ((doc && doc.scrollTop) || (body && body.scrollTop) || 0);
+        if (scrollTop > max && isFixed === false) {
+            isFixed = true;
+            el.style.marginTop = el.style.top = '-113px';
+            el.className = 'masthead masthead-fixed';
+            setTimeout(function () {
+                el.style.marginTop = el.style.top = '0';
+            }, 200);
+            contentEl.style.marginTop = '113px';
+        } else if (scrollTop <= max && isFixed === true) {
+            isFixed = false;
+            el.className = 'masthead sixteen columns';
+            el.style.marginTop = el.style.top = '';
+            contentEl.style.marginTop = '0';
+        }
+    };
+
+window.onscroll = function (e) {
+    'use strict';
+    clearTimeout(timer);
+    timer = setTimeout(setFixedHeader, 100);
+};
