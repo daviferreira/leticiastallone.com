e381a186eb2096aa35b6ad7f657271684da14642
Davi Ferreira <contato@daviferreira.com>
2013-06-15 19:16:35 -0300

    Changes article CSS, fixes pdf upload

--- leticiastallone/publications/models.py
+++ leticiastallone/publications/models.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.db import models
 
 from content.models import Content
@@ -7,5 +6,5 @@ from content.models import Content
 class Article(Content, models.Model):
     authors = models.TextField()
     journal = models.CharField(max_length=255)
-    pdf_file = models.FileField(upload_to=settings.MEDIA_ROOT, blank=True, null=True)
+    pdf_file = models.FileField(upload_to='articles', blank=True, null=True)
     link = models.CharField(max_length=255, blank=True, null=True)
