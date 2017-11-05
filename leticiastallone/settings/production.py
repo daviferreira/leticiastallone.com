371a709511a267ea787c19454d5da28ea18e96d4
Davi Ferreira <contato@daviferreira.com>
2017-11-05 17:41:31 +0100

    Debug

--- leticiastallone/settings/production.py
+++ leticiastallone/settings/production.py
@@ -20,6 +20,14 @@ def get_env_setting(setting):
 
 INSTALLED_APPS += ('gunicorn',)
 
+########## DEBUG CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
+TEMPLATE_DEBUG = DEBUG
+########## END DEBUG CONFIGURATION
+
 ########## EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
